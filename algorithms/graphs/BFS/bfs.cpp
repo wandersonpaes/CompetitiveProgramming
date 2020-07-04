ea38c28542ac26fc892cff59e8ce6e089d84be5a
wandersonpaes <wandersonpaeswop3@gmail.com>
2020-07-04 16:50:25 -0300

    breadth-first search algorithm

---
+++ algorithms/graphs/BFS/bfs.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int vertice, edge;
+   cin >> vertice >> edge;
+   vector < vector <int> > graph(vertice+1);
+
+   //initializing a graph
+   for(int i=0; i<edge; i++){
+      int from, to;
+      cin >> from >> to;
+
+      graph[from].push_back(to);
+      graph[to].push_back(from);
+   }
+
+   //breadth-firts search
+   int source=1;
+   vector <bool> processed(vertice+1);
+   queue <int> queueGraph;
+   queueGraph.push(source);
+
+   cout << source << " ";
+   while(!queueGraph.empty()){
+      int dad = queueGraph.front();
+      queueGraph.pop();
+      processed[dad]=true;
+
+      for(auto son : graph[dad]){
+
+         if(!processed[son]){
+            cout << son << " ";
+            queueGraph.push(son);
+            processed[son]=true;
+         }
+      }
+   }cout << endl;
+
+   return 0;
+}
