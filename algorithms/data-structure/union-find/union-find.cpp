4ad300afa3616fb7833cb2968bebd1cf0be99918
wandersonpaes <wandersonpaeswop3@gmail.com>
2020-07-08 23:33:13 -0300

    union-find, data-structure

---
+++ algorithms/data-structure/union-find/union-find.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define MAXN 100100
+
+using namespace std;
+
+int dad[MAXN], dist[MAXN], amount[MAXN];
+
+int find(int x){
+   if(dad[x]==x){
+      return x;
+   }
+   return dad[x]=find(dad[x]);
+}
+
+void join(int x, int y){
+   x = find(x);
+   y = find(y);
+
+   if(x==y) return;
+   
+   //analyze distance array to choice the best dad 
+   //and upgrade amount array
+   if(dist[x]<dist[y]){
+      dad[x]=y;
+      amount[y]+=amount[x];
+   }
+   if(dist[x]>dist[y]){
+      dad[y]=x;
+      amount[x]+=amount[y];
+   }
+   if(dist[x]==dist[y]){
+      dad[x]=y;
+      dist[y]++;
+      amount[y]+=amount[x];
+   }
+
+}
+
+int main(){
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int n, k; //nodes and operation number
+   cin >> n >> k;
+
+   //initialize arrays
+   for(int i=1; i<=n; i++){
+      dad[i]=i;
+      dist[i]=0;
+      amount[i]=1;
+   }
+
+   for(int i=0; i<k; i++){
+      char op; //operation type
+      int a, b; //nodes
+      cin >> op >> a >> b;
+
+      //Q = query and F = fusion
+      if(op == 'Q'){
+         (find(a) == find(b)) ? cout << "Y\n" : cout << "N\n";
+      }else{
+         join(a,b);
+      }
+   }
+
+   return 0;
+}
